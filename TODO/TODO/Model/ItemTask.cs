9409aa00cff1182fd43231c322b307d59beba09d
Elias Pujols <eliasnurbs@gmail.com>
2018-12-27 09:31:14 -0400

    Primarykey or Id for the item.

--- TODO/TODO/Model/ItemTask.cs
+++ TODO/TODO/Model/ItemTask.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+using SQLite;
+
 namespace TODO.Model
 {
     public class ItemTask
     {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Date { get; set; }
         public bool IsCompleted { get; set; }
