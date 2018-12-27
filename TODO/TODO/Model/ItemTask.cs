fdd325252c6f6dec527a19aeb8b02b36cb3a8513
Elias Pujols <eliasnurbs@gmail.com>
2018-12-27 08:53:47 -0400

    Task model added.

---
+++ TODO/TODO/Model/ItemTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO.Model
+{
+    public class ItemTask
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Color { get; set; }
+    }
+}
